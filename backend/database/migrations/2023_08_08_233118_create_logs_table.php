2aa1e495f69639fedaedddb6f89251f44f865cca
enam-0042 <enamulhaque0042@gmail.com>
2023-08-09 15:10:56 +0600

    "with database"

---
+++ backend/database/migrations/2023_08_08_233118_create_logs_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('logs', function (Blueprint $table) {
+            $table->id();
+            $table->string('execution_type');
+            $table->string('old_lable')->default(false)->nullable();
+            $table->string('new_lable')->default(false)->nullable();
+            $table->string('login_email');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('logs');
+    }
+};
