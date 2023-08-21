4c240d36804eaacf2dc481d2d5d3094041081815
Enamul Haque <32878813+enam-0042@users.noreply.github.com>
2023-08-21 18:32:38 +0600

    Add files via upload
    frontend base created

---
+++ frontend/src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Signin from './pages/signin'
+import Signup from './pages/signup'
+import Home from './pages/home'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      {/* <Signin/> */}
+      {/* <Signup/> */}
+      <Home/>
+    </>
+  )
+}
+
+export default App
