db12b44255ac3e2daee2c506b060c5ee8456410c
enam-0042 <enamulhaque0042@gmail.com>
2023-08-28 18:31:48 +0600

    "edit-ip "

---
+++ frontend/src/components/edit-ip.jsx
@@ -0,0 +1,136 @@
+import { Fragment, useRef, useState } from "react";
+import { Dialog, Transition } from "@headlessui/react";
+import { send } from "../api/globalFetch";
+import Loader from "../components/loader";
+function Editip({ open, setOpen, selectedIp }) {
+  const [loading, setLoading] = useState(false);
+
+  const [formData, setFormData] = useState({
+    ip_address: selectedIp.ip_address,
+    label: selectedIp.label,
+    id: selectedIp.id,
+  });
+  const handleChange = (e) => {
+    let name = e.target.name;
+    let value = e.target.value;
+
+    setFormData({ ...formData, [name]: value });
+  };
+
+  async function handleSubmit(event) {
+    setLoading(true);
+    event.preventDefault();
+    //  console.log(formData);
+    const responseData = await send(`ip_lists/${selectedIp.id}`, {...selectedIp, ...formData},'PUT');
+    console.log(responseData);
+    setLoading(false);
+    setOpen(false);
+  }
+
+  const cancelButtonRef = useRef(null);
+  return (
+    <Transition.Root show={open} as={Fragment}>
+      <Dialog
+        as="div"
+        className="relative z-10"
+        initialFocus={cancelButtonRef}
+        onClose={setOpen}
+      >
+        <Transition.Child
+          as={Fragment}
+          enter="ease-out duration-300"
+          enterFrom="opacity-0"
+          enterTo="opacity-100"
+          leave="ease-in duration-200"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
+        >
+          <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+        </Transition.Child>
+
+        <div className="fixed inset-0 z-10 overflow-y-auto">
+          <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+              enterTo="opacity-100 translate-y-0 sm:scale-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+              leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+            >
+              <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all">
+                <div className="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4 w-96">
+                  <div className="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
+                    <Dialog.Title
+                      as="h3"
+                      className="w-full text-base font-semibold leading-6 text-gray-900"
+                    >
+                      Add New IP
+                    </Dialog.Title>
+                    <div className="mt-2">
+                      <div className="text-sm text-gray-500">
+                        <form className="space-y-6" onSubmit={handleSubmit}>
+                          <div>
+                            <label
+                              htmlFor="ip_address"
+                              className="text-left block text-sm font-medium leading-6 text-gray-900"
+                            >
+                              IP address
+                            </label>
+                            <div className="mt-2">
+                              <input
+                                id="ip_address"
+                                name="ip_address"
+                                type="text"
+                                required
+                                defaultValue={selectedIp.ip_address}
+                                value={formData.ip_address}
+                                readOnly
+                                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                              />
+                            </div>
+                          </div>
+                          <div>
+                            <label
+                              htmlFor="password"
+                              className="text-left block text-sm font-medium leading-6 text-gray-900"
+                            >
+                              Label
+                            </label>
+                            <div className="mt-2">
+                              <input
+                                id="label"
+                                name="label"
+                                type="text"
+                                defaultValue={selectedIp.label}
+                                value={formData.label}
+                                required
+                                onChange={handleChange}
+                                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                              />
+                            </div>
+                          </div>
+                          <div>
+                            <button
+                              type="submit"
+                              className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                            >
+                              {loading && <Loader />}
+                              Edit ip
+                            </button>
+                          </div>
+                        </form>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </Dialog.Panel>
+            </Transition.Child>
+          </div>
+        </div>
+      </Dialog>
+    </Transition.Root>
+  );
+}
+export default Editip;
--- frontend/src/components/iplists.jsx
+++ frontend/src/components/iplists.jsx
@@ -1,6 +1,9 @@
 import React from "react";
-
+import Editip from "./edit-ip";
+import { useState } from "react";
 function Iplists({ ipList }) {
+  const [open, setOpen] = useState(false);
+  const [selectedIp, setSelectedIp] = useState({});
   //console.log(ipList);
   return (
     <div>
@@ -23,14 +26,17 @@ function Iplists({ ipList }) {
             </tr>
           </thead>
           <tbody>
-            {ipList?.map((ip,index) => {
+            {ipList?.map((ip, index) => {
               return (
-                <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700" key={ip.id}>
+                <tr
+                  className="bg-white border-b dark:bg-gray-800 dark:border-gray-700"
+                  key={ip.id}
+                >
                   <th
                     scope="row"
                     className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
                   >
-                    {index+1}
+                    {index + 1}
                   </th>
                   <td className="px-6 py-4">{ip.ip_address}</td>
                   <td className="px-6 py-4">{ip.label}</td>
@@ -38,7 +44,12 @@ function Iplists({ ipList }) {
                     {" "}
                     <button
                       type="button"
-                      onClick={() => setOpen(true)}
+                      onClick={() => {
+                        setSelectedIp(ip);
+                        //console.log(ip);
+                        setOpen(true);
+                        
+                      }}
                       className="flex justify-center rounded-md bg-blue-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                     >
                       Edit
@@ -47,29 +58,9 @@ function Iplists({ ipList }) {
                 </tr>
               );
             })}
-
-            {/* <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-              <th
-                scope="row"
-                className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-              >
-                Apple MacBook Pro 17"
-              </th>
-              <td className="px-6 py-4">Silver</td>
-              <td className="px-6 py-4">Laptop</td>
-              <td className="px-6 py-4">
-                {" "}
-                <button
-                  type="button"
-                  onClick={() => setOpen(true)}
-                  className="flex justify-center rounded-md bg-blue-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                >
-                  Edit
-                </button>
-              </td>
-            </tr> */}
           </tbody>
         </table>
+        <Editip open={open} setOpen={setOpen} selectedIp={selectedIp}/>
       </div>
     </div>
   );
